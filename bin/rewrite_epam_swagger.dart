64981132b5ade8e6d3349b5fbcc2b8d245bc1be7
Nikita Titov <nik@titov.dev>
2024-10-31 10:33:06 -0500

    Initial commit

---
+++ bin/rewrite_epam_swagger.dart
@@ -0,0 +1,5 @@
+import 'package:rewrite_epam_swagger/rewrite_epam_swagger.dart' as rewrite_epam_swagger;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rewrite_epam_swagger.calculate()}!');
+}
---
+++ test/rewrite_epam_swagger_test.dart
@@ -0,0 +1,8 @@
+import 'package:rewrite_epam_swagger/rewrite_epam_swagger.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
